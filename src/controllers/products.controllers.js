eaec85606ad54bfc2f19e493d703e85725b02f47
Cristian Mozetich <cristianmozetich@gmail.com>
2023-11-05 19:13:44 +0000

    prod desde front

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -12,7 +12,7 @@ export const getProducts = async (req,res) => {
         const prods = await productModel.paginate({filter: filter}, {limit: lim, page: pag, sort: {price: ord}})
 
         if(prods){
-            return res.status(200).send(products)
+            return res.status(200).send(prods)
         } 
         res.status(404).send({error: "Productos no encontrados"})    
     }catch (error){
