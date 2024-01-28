a4a56932a9fbdab9e2676d571d5ee160aa6e94ef
Cristian Mozetich <cristianmozetich@gmail.com>
2024-01-28 17:05:30 +0000

    entrega final

--- src/controllers/orders.controllers.js
+++ src/controllers/orders.controllers.js
@@ -5,7 +5,7 @@ import 'dotenv/config'
 import Stripe from "stripe"
 
 //STRIPE
-
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
 
 
 //PurchaseCart NO FUNCIONA CORRECTAMENTE
@@ -68,10 +68,10 @@ export const purchaseCart = async (req, res) => {
 
 export const checkout = async (req, res) => {
 
-    const stripe = new Stripe(process.env.STRIPE, {
+    const stripe = new Stripe(stripeSecretKey, {
         apiVersion: '2023-10-16',
       })
-      
+
     const { id, amount, description } = req.body
 
     try{
