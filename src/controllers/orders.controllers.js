91b99efc056a8e7fec2efeb5fd810a433c81c09a
Cristian Mozetich <cristianmozetich@gmail.com>
2024-01-28 18:26:14 +0000

    entrega final

--- src/controllers/orders.controllers.js
+++ src/controllers/orders.controllers.js
@@ -5,7 +5,7 @@ import 'dotenv/config'
 import Stripe from "stripe"
 
 //STRIPE
-const stripeSecretKey = process.env.STRIPE
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
 const stripe = new Stripe(stripeSecretKey, {
     apiVersion: '2023-10-16',
   })
