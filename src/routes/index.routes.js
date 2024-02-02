246efd96105ae08a9de514d0a785f58377aa8de6
Cristian Mozetich <cristianmozetich@gmail.com>
2024-02-02 15:04:28 +0000

    render e imagenes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -14,7 +14,7 @@ const router = Router()
 //ROUTES
 router.use('/api/users', userRouter)
 // Configura express.static para manejar archivos estáticos en la ruta /api/users/images
-app.use('/api/users/images', express.static(path.join(__dirname, '../../upload/products')));
+router.use('/api/users/images', express.static(path.join(__dirname, '../../upload/products')));
 
 router.use('/api/products', productRouter)
 router.use('/api/carts', cartRouter)
