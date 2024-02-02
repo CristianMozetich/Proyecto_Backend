53c4ca08f5c095fbf8232e6100433b7e6079708b
Cristian Mozetich <cristianmozetich@gmail.com>
2024-02-02 16:08:16 +0000

    render e imagenes

--- src/controllers/img.controllers.js
+++ src/controllers/img.controllers.js
@@ -2,13 +2,13 @@ import path from 'path';
 import fs from 'fs';
 
 // Obtén la ruta del directorio del módulo actual
-const __filename = import.meta.url.substring("file:///".length);
-const __dirname = path.dirname(__filename);
+//const __filename = import.meta.url.substring("file:///".length);
+//const __dirname = path.dirname(__filename);
 
 const getImageById = (req, res) => {
   const { filename } = req.params;
   console.log(`Intento de acceder a la imagen: ${filename}`);
-  const imagePath = path.join(__dirname, 'upload/products', filename);
+  const imagePath = path.join('../../upload/products', filename);
 
   if (fs.existsSync(imagePath)) {
     res.sendFile(imagePath);
